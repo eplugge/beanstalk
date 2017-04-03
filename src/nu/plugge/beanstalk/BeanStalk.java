b4d3b33eddf25d7e3487d2d5d86882338ac2a6c0
Eelco Plugge <eelco@plugge.nu>
2017-04-03 20:07:59 +0200

    Established file system structure.

---
+++ src/nu/plugge/beanstalk/BeanStalk.java
@@ -0,0 +1,12 @@
+package nu.plugge.beanstalk
+
+import java.io.File;
+import java.io.IOException;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class BeanStalk {
+
+}
